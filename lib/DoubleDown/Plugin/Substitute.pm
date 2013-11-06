623ec21feb94b4766feba8133e1ff980835f0c9f
Steve Nolte <steve.nolte@corvisa.com>
2013-11-06 16:19:03 -0600

    Tidy and rid unused modules

--- lib/DoubleDown/Plugin/Substitute.pm
+++ lib/DoubleDown/Plugin/Substitute.pm
@@ -3,43 +3,37 @@ package DoubleDown::Plugin::Substitute;
 use Moose;
 use namespace::autoclean;
 
-require DoubleDown::Plugin::Rhyme::Word;
-
-use LWP::Simple;
-use JSON;
-
 has text_match => (
-  is => 'ro',
-  isa => 'HashRef',
-  builder => '_build_text_match'
+	is      => 'ro',
+	isa     => 'HashRef',
+	builder => '_build_text_match'
 );
 
 sub _build_text_match {
-  return {
-    '^(\S+)?\s*s\/(.+?)\/([^\/]+)' => {
-			func => 'process',
+	return {
+		'^(\S+)?\s*s\/(.+?)\/([^\/]+)' => {
+			func       => 'process',
 			match_desc => 's/orig/sub or <nick> s/orig/sub',
-			desc => 'Substitues original text with new text'
+			desc       => 'Substitues original text with new text'
 		},
-    '^.+$' => {
-			func => 'stash_last_msg',
+		'^.+$' => {
+			func       => 'stash_last_msg',
 			match_desc => '<text>',
-			desc => 'Stores last text entered in by any user'
+			desc       => 'Stores last text entered in by any user'
 		}
-  };
+	};
 }
-
 
 sub process {
 	my ( $self, $message ) = @_;
 
-	my $core = DoubleDown::Core->instance;
-	my $con = $core->irc->_con;
-	my $parts = $message->parts;
+	my $core    = DoubleDown::Core->instance;
+	my $con     = $core->irc->_con;
+	my $parts   = $message->parts;
 	my $channel = $message->channel;
-	my $nick = $message->nick;
+	my $nick    = $message->nick;
 
-  $message->text =~ m{
+	$message->text =~ m{
     ^(\S+)?
     \s*
     s/
@@ -48,24 +42,23 @@ sub process {
     ([^/]+)
   }x;
 
-  my $user     = $1 // $nick;
-  my $search   = $2;
-  my $replace  = $3;
-  my $last_msg = $core->stash->{last_msg}->{$channel}->{$user};
-  $core->debug(
-    message => sprintf(
-      'search and replace "%s" with "%s" in %s for %s in channel %s',
-    	$search, $replace, $last_msg, $nick, $channel
-    )
-  );
+	my $user     = $1 // $nick;
+	my $search   = $2;
+	my $replace  = $3;
+	my $last_msg = $core->stash->{last_msg}->{$channel}->{$user};
+	$core->debug(
+		message => sprintf(
+			'search and replace "%s" with "%s" in %s for %s in channel %s',
+			$search, $replace, $last_msg, $nick, $channel
+		)
+	);
 
-  my $fixed = $last_msg;
-  $fixed =~ s/$search/$replace/g;
+	my $fixed = $last_msg;
+	$fixed =~ s/$search/$replace/g;
 
-  $con->send_msg( undef, PRIVMSG => $channel, "<$user> " . $fixed ) if $fixed ne $last_msg;
-  $core->stash->{last_msg}->{$channel}->{$user} = $fixed;
+	$con->send_msg( undef, PRIVMSG => $channel, "<$user> " . $fixed ) if $fixed ne $last_msg;
+	$core->stash->{last_msg}->{$channel}->{$user} = $fixed;
 }
-
 
 sub stash_last_msg {
 
@@ -77,11 +70,11 @@ sub stash_last_msg {
 
 	my $core = DoubleDown::Core->instance;
 
-  $core->stash->{last_msg}->{$channel}->{$nick} = $message->text;
-  $core->debug(
-    message => sprintf( 'received public message %s in channel %s from %s', $message->text, $channel, $nick ),
-    color   => $core->debug_cyan
-  );
+	$core->stash->{last_msg}->{$channel}->{$nick} = $message->text;
+	$core->debug(
+		message => sprintf( 'received public message %s in channel %s from %s', $message->text, $channel, $nick ),
+		color   => $core->debug_cyan
+	);
 }
 
 __PACKAGE__->meta->make_immutable;
